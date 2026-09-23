b27800eef70fecd4f08fd98545f5bdd5ac2963c9
Jaime R. Salinas Jr <jaime.salinas08@utrgv.edu>
2026-09-23 13:45:32 -0500

    update code

--- js/example.js
+++ js/example.js
@@ -2,12 +2,29 @@ $(function() {
   
   // TODO: Create a list of JS Object each representing a game. 
   // Use the data contained in the <ul> to build this list of game titles.
-
+  
+  const games = [
+    {
+      "title": "The Legend of Zelda: Breath of the Wild"
+    },
+    {
+      "title": "God of War Ragnarök"
+    },
+    {
+      "title": "Halo Infinite"
+    },
+    {
+      "title": "Minecraft"
+    },
+    {
+      "title": "Super Mario Odyssey"
+    }
+];
 
   var gameList, newItemForm, newItemButton;
   var item = '';                                 
   
-  
+  // const ulObj = document.getElementById("gameList");
   gameList = $('ul');                               
   newItemForm = $('#newItemForm');              
   newItemButton = $('#newItemButton');          
@@ -17,6 +34,14 @@ $(function() {
   // create a new node "list item" holding the game title and 
   // inject the new node inside the <ul>.
 
+  function loadGames() {
+    games.forEach( game => {
+      const listItem = $("<li>");
+      listItem.html(`${game.title}`);
+      gameList.append(listItem);
+    });
+  }
+  loadGames();
 
 
   function updateCount() {                      
